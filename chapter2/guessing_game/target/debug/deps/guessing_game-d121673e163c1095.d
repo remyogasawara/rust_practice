ffc07d6fda8b9b385e9e66a959ca99687b6f500c
remyogasawara <67338690+remyogasawara@users.noreply.github.com>
2024-03-08 21:46:07 -0800

    Delete chapter2/guessing_game/target directory

--- chapter2/guessing_game/target/debug/deps/guessing_game-d121673e163c1095.d
+++
@@ -1,5 +0,0 @@
-/Users/remyogasawara/Desktop/rust/rust_practice/guessing_game/target/debug/deps/guessing_game-d121673e163c1095: src/main.rs
-
-/Users/remyogasawara/Desktop/rust/rust_practice/guessing_game/target/debug/deps/guessing_game-d121673e163c1095.d: src/main.rs
-
-src/main.rs:
